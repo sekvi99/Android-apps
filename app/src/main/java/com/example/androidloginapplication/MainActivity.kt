cd8b45f9db290d2cdc7a430afd7c37b72ddbcb76
sekvi99 <filmatkozlik@gmail.com>
2023-05-24 13:15:58 +0200

    Modified mainacivity for created xml views

--- app/src/main/java/com/example/androidloginapplication/MainActivity.kt
+++ app/src/main/java/com/example/androidloginapplication/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.androidloginapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var usernameTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_layout)
+
+        usernameEditText = findViewById(R.id.usernameEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                setContentView(R.layout.home_layout)
+                usernameTextView = findViewById(R.id.usernameTextView)
+                usernameTextView.text = "Logged in as: $username"
+            }
+        }
     }
 }
